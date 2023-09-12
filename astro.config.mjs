98bb3c15fced69a66844d172f0c02fdb470f101b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-09-12 18:52:43 +0200

    main : project init

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+
+export default defineConfig({
+  site: 'https://nuoxoxo.github.io',
+  base: '/pokemon', // don't set if no path after `io/`
+})
